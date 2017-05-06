94e05d99b57f71d8e0c2baa0b1dccc1964f8eee4
Zack Goldberg <zsgoldberg@gmail.com>
2017-05-06 18:42:31 -0400

    card components by cli

--- fe/chinese-poker/src/app/app.module.ts
+++ fe/chinese-poker/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
