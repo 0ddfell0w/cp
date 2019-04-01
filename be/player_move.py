f176f8603ac049d760636e4179e22d87f440e2d5
Zack Goldberg <zsgoldberg@gmail.com>
2019-03-31 21:37:32 -0400

    player_move += from string

--- be/player_move.py
+++ be/player_move.py
@@ -1,4 +1,19 @@
+from .card_collection import CardCollection
+from .move_factory import get_move
+
+
 class PlayerMove:
   def __init__(self, player, move):
     self.player = player
     self.move = move
+
+  @staticmethod
+  def from_string(player, string):
+    cards = CardCollection.from_string(string)
+    invalid_cards_for_player = set(cards) - set(player.cards)
+    if invalid_cards_for_player:
+      raise ValueError(
+        "Player {} does not have {} in their hand".format(
+          player, ",".join(map(str, invalid_cards_for_player))))
+
+    return PlayerMove(player, get_move(cards))
